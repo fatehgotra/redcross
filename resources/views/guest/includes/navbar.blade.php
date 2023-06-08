09d76225a27aa3679f5a024b501e0e969ed12e88
Prashant Chauhan <rc21292@gmail.com>
2023-06-08 20:50:15 +0530

    uploading all files

--- resources/views/guest/includes/navbar.blade.php
+++ resources/views/guest/includes/navbar.blade.php
@@ -3,7 +3,7 @@
 
         <!-- logo -->
         <a href="/" class="navbar-brand me-lg-4">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="" class="logo-dark"/>
+            <img src="{{ asset('assets/images/logo.png') }}" alt="" class="logo-dark" />
         </a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -24,21 +24,46 @@
                 </li>
                 <li class="nav-item mx-lg-1">
                     <a class="nav-link" href="">Contact Us</a>
-                </li>               
+                </li>
+                @auth
+                <li class="nav-item mx-lg-1">
+                    <a href="{{ route('home') }}" class="nav-link d-lg-none">Dashboard</a>
+                </li>
+                <li class="nav-item mx-lg-1">
+                    <a href="{{ route('login') }}"
+                    onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();"
+                    class="nav-link d-lg-none">Logout</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+                @else
+                @if (Route::has('login'))
+                    <li class="nav-item mx-lg-1">
+                        <a href="{{ route('login') }}" class="nav-link d-lg-none">Login</a>
+                    </li>
+                    <li class="nav-item mx-lg-1">
+                        <a href="{{ route('admin.login') }}" class="nav-link d-lg-none">Department Login</a>
+                    </li>
+                @endif
+                @if (Route::has('register'))
+                    <li class="nav-item mx-lg-1">
+                        <a href="{{ route('lodge-information.form') }}" class="nav-link d-lg-none">Volunteer Now</a>
+                    </li>
+                @endif
+                @endauth
             </ul>
 
             <!-- right menu -->
             @auth
                 <ul class="navbar-nav ms-auto align-items-center">
                     <li class="nav-item me-0">
-                        <a href="{{ route('home') }}" class="nav-link d-lg-none">Dashboard</a>
+                       
                         <a href="{{ route('home') }}" class="btn btn-sm btn-info btn-rounded d-none d-lg-inline-flex">
                             <i class="mdi mdi-home me-1"></i> Dashboard
                         </a>
-                        <a href="{{ route('login') }}"
-                            onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();"
-                            class="nav-link d-lg-none">Logout</a>
+                       
                         <a href="{{ route('login') }}"
                             onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();"
@@ -54,17 +79,15 @@
                 <ul class="navbar-nav ms-auto align-items-center">
                     <li class="nav-item me-0">
                         @if (Route::has('login'))
-                            <a href="{{ route('login') }}" class="nav-link d-lg-none">Login</a>
                             <a href="{{ route('login') }}" class="btn btn-sm btn-outline-light d-none d-lg-inline-flex">
-                                 Login
+                                Login
                             </a>
-                            <a href="{{ route('admin.login') }}" class="nav-link d-lg-none">Department Login</a>
-                            <a href="{{ route('admin.login') }}" class="btn btn-sm btn-outline-light d-none d-lg-inline-flex">
-                            Department Login
+                            <a href="{{ route('admin.login') }}"
+                                class="btn btn-sm btn-outline-light d-none d-lg-inline-flex">
+                                Department Login
                             </a>
                         @endif
-                        @if (Route::has('register'))
-                            <a href="{{ route('lodge-information.form') }}" class="nav-link d-lg-none">Volunteer Now</a>
+                        @if (Route::has('register'))                            
                             <a href="{{ route('lodge-information.form') }}"
                                 class="btn btn-sm btn-outline-info d-none d-lg-inline-flex">
                                 <i class="mdi mdi-water text-danger me-1"></i>Volunteer Now
