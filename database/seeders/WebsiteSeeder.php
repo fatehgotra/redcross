880f33385f06c1aef705ab435c2cfc3760a9705b
Prashant Chauhan <rc21292@gmail.com>
2023-07-12 17:38:16 +0530

    Uploading all files

--- database/seeders/WebsiteSeeder.php
+++ database/seeders/WebsiteSeeder.php
@@ -21,6 +21,7 @@ class WebsiteSeeder extends Seeder
         Role::create(['guard_name' => 'admin', 'name' => 'branch-level']);
         Role::create(['guard_name' => 'admin', 'name' => 'division-manager']);
         Role::create(['guard_name' => 'admin', 'name' => 'hq']);
+        Role::create(['guard_name' => 'admin', 'name' => 'course-coordinator']);
 
             $branches[0] = "Rotuma";
             $branches[1] = "Levuka";
@@ -102,5 +103,16 @@ class WebsiteSeeder extends Seeder
             $multiple_roles->assignRole('division-manager');
             $multiple_roles->assignRole('hq');
         }
+
+        $coordinator = Admin::create([
+            'name'              => 'John Doe',
+            'email'             => 'course_coordinator@admin.com',
+            'phone'             => '9876543610',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+        ]);
+
+        $coordinator->assignRole('course-coordinator');
     }
 }
